b6ffceb0c401754bc3de019d5680b8a93d26556d
OK Bear <supernova1111a@outlook.com>
2025-05-09 13:11:25 -0700

    origin

--- next.config.mjs
+++ next.config.mjs
@@ -9,34 +9,6 @@ const nextConfig = {
         }
       ]
   },
-  async rewrites() {
-    return [
-      {
-        source: '/dynamic-auth/:path*',
-        destination: 'https://app.dynamicauth.com/api/v0/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'content-type',
-          },
-        ],
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/dynamic-auth/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, Origin' },
-          { key: 'Content-Type', value: 'application/json' },
-        ],
-      },
-    ]
-  },
   images: {
     domains: ['*'],
     unoptimized: true
--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -4,18 +4,21 @@ import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 
+
 export default function Providers({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  
+
   return (
     <DynamicContextProvider
       theme="auto"
       settings={{
         environmentId: "036ecc71-d538-4f6c-8e7a-9c5324208c7e",
         walletConnectors: [EthereumWalletConnectors, SolanaWalletConnectors],
-        apiBaseUrl: "/dynamic-auth"
       }}
     >
       {children}
